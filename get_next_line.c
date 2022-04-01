ed31890729d0b0d40fe34ba34841893da7de3992
Sasha-rgb <smikayel@c2r1s5.42yerevan.am>
2022-04-01 20:27:07 +0400

    asd

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,4 @@
-
-# define BUFFER_SIZE 42
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-#include <stddef.h>
+#include "get_next_line.h"
 
 size_t	ft_strlen(const char *str)
 {
@@ -18,7 +12,7 @@ size_t	ft_strlen(const char *str)
 	return (len);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*joined_str;
 	int		i;
@@ -43,6 +37,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	joined_str[j] = '\0';
+	free(s1);
 	return (joined_str);
 }
 
@@ -75,22 +70,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (sub_str);
 }
 
-int endl_indx(char *saved)
-{
-	int index;
-
-	index = 0;
-	while (saved[index] != '\0')
-	{
-		if (saved[index] == '\n')
-			return (index);
-		index++;
-	}
-	return (-1);
-}
-
-
-
 char	*ft_strdup(const char *src)
 {
 	char	*dup;
@@ -109,46 +88,106 @@ char	*ft_strdup(const char *src)
 	return (dup);
 }
 
+//===================================
+
+
+
+int is_endl(char *saved)
+{
+	int index;
+
+	index = 0;
+	while (saved[index] != '\0')
+	{
+		if (saved[index] == '\n')
+			return (1);
+		index++;
+	}
+	return (0);
+}
+
+int endl_index(char *stash)
+{
+	int i;
+
+	i = 0;
+	while (stash[i] != '\0')
+	{
+		if(stash[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
 char *get_next_line(int fd)
 {
 	int bytes;
 	char buff[BUFFER_SIZE + 1];
-	static char *saved;
+	static char *stash;
 	char *line;
-	int endl;
-	
-	while (1)
+	char *ptr;
+
+	while(1)
 	{
 		bytes = read(fd, buff, BUFFER_SIZE);
+		buff[bytes] = '\0';
 		if (bytes == -1)
-		{
 			return (NULL);
+		if (bytes > 0)
+		{
+			if(!stash)
+				stash = ft_strdup(buff);
+			else
+				stash = ft_strjoin(stash, buff);
+			if (is_endl(stash) == 1)
+				break ;
 		}
-		if (bytes == 0)
-			??????????
-		buff[BUFFER_SIZE] = '\0';
-		if (!saved)
-			saved = ft_strdup(buff);
 		else
-			saved = ft_strjoin(saved, buff);
-		endl = endl_indx(saved);
-		if (endl != -1)
-			break ;
+		{
+			if (!stash)
+				return (NULL);
+			else
+			{
+				if (is_endl(stash) == 1)
+				{
+					line = ft_substr(stash, 0, endl_index(stash) + 1);
+					ptr = stash;
+					stash = ft_substr(stash, endl_index(stash) + 1, ft_strlen(stash));
+					free(ptr);
+					return (line);
+				}
+				else
+				{
+					line = ft_strdup(stash);
+					ptr = stash;
+					free(stash);
+					stash = NULL;
+					return (line);
+				}
+			}
+		}
 	}
-	line = ft_substr(saved, 0, endl + 1);
-	saved = ft_substr(saved, endl + 1, ft_strlen(saved));
+
+	line = ft_substr(stash, 0, endl_index(stash) + 1);
+	ptr = stash;
+	stash = ft_substr(stash, endl_index(stash) + 1, ft_strlen(stash));
+
+
+	free(ptr);
 	return (line);
 }
 
 
 
 
-int main()
-{
-	int fd;
-	int i = 55;
+// int main()
+// {
+// 	int fd;
+// 	int i = 5;
 
-	fd = open("text.txt", O_RDONLY);
-	while(i--)
-		printf(">>>>%s",get_next_line(fd));
-}
+// 	fd = open("nl", O_RDONLY);
+// 	while(i--)
+// 		printf("%s",get_next_line(fd));
+// 		//get_next_line(fd);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: smikayel <smikayel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 19:12:31 by smikayel          #+#    #+#             */
-/*   Updated: 2022/03/30 13:47:21 by smikayel         ###   ########.fr       */
+/*   Updated: 2022/04/01 16:58:18 by smikayel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10000
+# define BUFFER_SIZE 42
 #endif
 
 # include <unistd.h>
@@ -25,7 +25,7 @@
 
 char	*ft_strdup(const char *src);
 size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
 
